25d87b9294d3885a4b03ffd7e20e9dfd4aad2022
zetrasystemsq8-beep <zetra.systems.q8@gmail.com>
2026-05-31 22:35:23 +0100

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/flutter_video_feed/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_video_feed/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.flutter_video_feed
+package com.nigergram.app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity()
