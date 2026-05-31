a3407b7854a523e0e1dee79c3295e3c401b6fd80
zetrasystemsq8-beep <zetra.systems.q8@gmail.com>
2026-05-31 20:02:06 +0100

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) throw UnsupportedError('Web not supported');
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      default:
+        throw UnsupportedError('Platform not supported');
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyCLcQqsHrq9O0sMKAs5g1hdv7KqzvEb4zo',
+    appId: '1:378525386177:android:e05705ba99d82c88c5db40',
+    messagingSenderId: '378525386177',
+    projectId: 'nigergram',
+    storageBucket: 'nigergram.firebasestorage.app',
+  );
+}
