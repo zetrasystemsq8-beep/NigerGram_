316ea4582fa1ab6b555e9fecc0b752e5e2a62e57
zetrasystemsq8-beep <zetra.systems.q8@gmail.com>
2026-06-03 19:26:32 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,12 @@ import 'package:nigergram/core/di/dependency_injector.dart';
 import 'package:nigergram/core/init/app_widget.dart';
 import 'package:nigergram/firebase_options.dart';
 
-Future<void> main() async {
-  // Ensure Flutter bindings are initialized before calling native code
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Firebase using the generated options
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // Setup your dependency injection (GetIt or similar)
-  await injectionSetup();
+  injectionSetup();
 
-  // Run the root widget
   runApp(const AppWidget());
 }
