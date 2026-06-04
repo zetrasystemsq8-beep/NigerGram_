cc41f661b00b2522719456df75291c9468bd44af
zetrasystemsq8-beep <zetra.systems.q8@gmail.com>
2026-06-04 20:54:52 +0100

    Create auth_state.dart

---
+++ lib/features/auth/presentation/bloc/auth_state.dart
@@ -0,0 +1,14 @@
+part of 'auth_cubit.dart';
+
+abstract class AuthState {}
+
+class AuthInitial extends AuthState {}
+
+class AuthLoading extends AuthState {}
+
+class AuthSuccess extends AuthState {}
+
+class AuthError extends AuthState {
+  final String message;
+  AuthError(this.message);
+}
