d3ee382c472ac8869157ca4c075dfea7b5cc6bff
zetrasystemsq8-beep <zetra.systems.q8@gmail.com>
2026-06-04 21:05:04 +0100

    Update app_router.dart

--- lib/core/init/router/app_router.dart
+++ lib/core/init/router/app_router.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_video_feed/core/design_system/colors.dart';
-import 'package:flutter_video_feed/core/design_system/widgets/bottom_navigation_widget.dart';
-import 'package:flutter_video_feed/core/init/router/custom_page_builder_widget.dart';
-import 'package:flutter_video_feed/core/utils/constants/enums/router_enum.dart';
-import 'package:flutter_video_feed/features/dashboard/presentation/view/dashboard_view.dart';
-import 'package:flutter_video_feed/features/profile/presentation/view/profile_view.dart';
-import 'package:flutter_video_feed/features/video_feed/presentation/view/video_feed_view.dart';
+import 'package:nigergram/core/design_system/colors.dart';
+import 'package:nigergram/core/design_system/widgets/bottom_navigation_widget.dart';
+import 'package:nigergram/core/init/router/custom_page_builder_widget.dart';
+import 'package:nigergram/core/utils/constants/enums/router_enum.dart';
+import 'package:nigergram/features/dashboard/presentation/view/dashboard_view.dart';
+import 'package:nigergram/features/profile/presentation/view/profile_view.dart';
+import 'package:nigergram/features/video_feed/presentation/view/video_feed_view.dart';
+import 'package:nigergram/features/auth/presentation/view/login_page.dart';
 import 'package:go_router/go_router.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
@@ -16,18 +17,21 @@ class AppRouter {
     navigatorKey: _rootNavigatorKey,
     initialLocation: RouterEnum.dashboardView.routeName,
     routes: [
+      GoRoute(
+        path: '/login',
+        pageBuilder: (context, state) => customPageBuilderWidget(context, state, const LoginPage()),
+      ),
       ShellRoute(
         navigatorKey: _shellNavigatorKey,
-        pageBuilder:
-            (context, state, child) => customPageBuilderWidget(
-              context,
-              state,
-              BottomNavigationWidget(
-                location: state.uri.toString(),
-                backgroundColor: state.uri.toString() == RouterEnum.videoFeedView.routeName ? black : null,
-                child: child,
-              ),
-            ),
+        pageBuilder: (context, state, child) => customPageBuilderWidget(
+          context,
+          state,
+          BottomNavigationWidget(
+            location: state.uri.toString(),
+            backgroundColor: state.uri.toString() == RouterEnum.videoFeedView.routeName ? black : null,
+            child: child,
+          ),
+        ),
         routes: [
           GoRoute(
             path: RouterEnum.dashboardView.routeName,
