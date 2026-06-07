29b6bbb69d835fa10b5913075560bf77d9171f35
zetrasystemsq8-beep <zetra.systems.q8@gmail.com>
2026-06-07 12:53:44 +0100

    Update app_router.dart

--- lib/core/init/router/app_router.dart
+++ lib/core/init/router/app_router.dart
@@ -1,12 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:nigergram/core/design_system/colors.dart';
 import 'package:nigergram/core/design_system/widgets/bottom_navigation_widget.dart';
 import 'package:nigergram/core/init/router/custom_page_builder_widget.dart';
 import 'package:nigergram/core/utils/constants/enums/router_enum.dart';
+import 'package:nigergram/features/auth/presentation/view/login_page.dart';
 import 'package:nigergram/features/dashboard/presentation/view/dashboard_view.dart';
 import 'package:nigergram/features/profile/presentation/view/profile_view.dart';
 import 'package:nigergram/features/video_feed/presentation/view/video_feed_view.dart';
-import 'package:nigergram/features/auth/presentation/view/login_page.dart';
 import 'package:go_router/go_router.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
@@ -15,11 +16,19 @@ final GlobalKey<NavigatorState> _shellNavigatorKey = GlobalKey<NavigatorState>(d
 class AppRouter {
   final router = GoRouter(
     navigatorKey: _rootNavigatorKey,
-    initialLocation: RouterEnum.dashboardView.routeName,
+    initialLocation: '/login',
+    redirect: (context, state) {
+      final loggedIn = FirebaseAuth.instance.currentUser != null;
+      final onLogin = state.matchedLocation == '/login';
+      if (!loggedIn && !onLogin) return '/login';
+      if (loggedIn && onLogin) return RouterEnum.dashboardView.routeName;
+      return null;
+    },
     routes: [
       GoRoute(
         path: '/login',
-        pageBuilder: (context, state) => customPageBuilderWidget(context, state, const LoginPage()),
+        pageBuilder: (context, state) =>
+            customPageBuilderWidget(context, state, const LoginPage()),
       ),
       ShellRoute(
         navigatorKey: _shellNavigatorKey,
@@ -28,22 +37,28 @@ class AppRouter {
           state,
           BottomNavigationWidget(
             location: state.uri.toString(),
-            backgroundColor: state.uri.toString() == RouterEnum.videoFeedView.routeName ? black : null,
+            backgroundColor:
+                state.uri.toString() == RouterEnum.videoFeedView.routeName
+                    ? black
+                    : null,
             child: child,
           ),
         ),
         routes: [
           GoRoute(
             path: RouterEnum.dashboardView.routeName,
-            pageBuilder: (context, state) => customPageBuilderWidget(context, state, const DashboardView()),
+            pageBuilder: (context, state) => customPageBuilderWidget(
+                context, state, const DashboardView()),
           ),
           GoRoute(
             path: RouterEnum.videoFeedView.routeName,
-            pageBuilder: (context, state) => customPageBuilderWidget(context, state, const VideoFeedView()),
+            pageBuilder: (context, state) => customPageBuilderWidget(
+                context, state, const VideoFeedView()),
           ),
           GoRoute(
             path: RouterEnum.profileView.routeName,
-            pageBuilder: (context, state) => customPageBuilderWidget(context, state, const ProfileView()),
+            pageBuilder: (context, state) => customPageBuilderWidget(
+                context, state, const ProfileView()),
           ),
         ],
       ),
