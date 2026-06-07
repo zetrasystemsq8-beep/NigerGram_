78cc1ddceab94028d11efc971cc45f702e1e55b9
zetrasystemsq8-beep <zetra.systems.q8@gmail.com>
2026-06-07 16:58:44 +0100

    Initialize Supabase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:nigergram/core/di/dependency_injector.dart';
 import 'package:nigergram/core/init/app_widget.dart';
 import 'package:nigergram/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await Supabase.initialize(
+    url: 'https://ssmwuihkafrulmvtiuam.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzbXd1aWhrYWZydWxtdnRpdWFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4Mjk2NjAsImV4cCI6MjA5NjQwNTY2MH0.e1PxmDW77ZhbonS-Z96SWA_sPyVGedzpZNZbJQz7pQo',
+  );
 
   injectionSetup();
 
