538531422c2158ba36b97332a30764e32feabbfd
zetrasystemsq8-beep <zetra.systems.q8@gmail.com>
2026-06-09 13:12:29 +0100

    Update dependency_injector.dart

--- lib/core/di/dependency_injector.dart
+++ lib/core/di/dependency_injector.dart
@@ -1,10 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_video_feed/core/init/router/app_router.dart';
-import 'package:flutter_video_feed/features/video_feed/data/repository_impl/video_feed_repository_impl.dart';
-import 'package:flutter_video_feed/features/video_feed/domain/repositories/video_feed_repository.dart';
-import 'package:flutter_video_feed/features/video_feed/domain/usecases/fetch_more_videos_usecase.dart';
-import 'package:flutter_video_feed/features/video_feed/domain/usecases/fetch_videos_usecase.dart';
-import 'package:flutter_video_feed/features/video_feed/presentation/bloc/video_feed_cubit.dart';
+import 'package:nigergram/core/init/router/app_router.dart';
+import 'package:nigergram/features/video_feed/data/repository_impl/video_feed_repository_impl.dart';
+import 'package:nigergram/features/video_feed/domain/repositories/video_feed_repository.dart';
+import 'package:nigergram/features/video_feed/domain/usecases/fetch_more_videos_usecase.dart';
+import 'package:nigergram/features/video_feed/domain/usecases/fetch_videos_usecase.dart';
+import 'package:nigergram/features/video_feed/presentation/bloc/video_feed_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
