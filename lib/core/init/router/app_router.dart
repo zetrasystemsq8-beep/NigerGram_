af84a53550383fbbb612b3400ccdf3bae76dc59c
zetrasystemsq8-beep <zetra.systems.q8@gmail.com>
2026-06-09 23:26:35 +0100

    Update app_router.dart

--- lib/core/init/router/app_router.dart
+++ lib/core/init/router/app_router.dart
@@ -1,35 +1,35 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:nigergram/core/design_system/colors.dart';
 import 'package:nigergram/core/design_system/widgets/bottom_navigation_widget.dart';
 import 'package:nigergram/core/init/router/custom_page_builder_widget.dart';
 import 'package:nigergram/core/utils/constants/enums/router_enum.dart';
 import 'package:nigergram/features/auth/presentation/view/login_page.dart';
+import 'package:nigergram/features/auth/presentation/view/register_page.dart';
 import 'package:nigergram/features/dashboard/presentation/view/dashboard_view.dart';
 import 'package:nigergram/features/profile/presentation/view/profile_view.dart';
 import 'package:nigergram/features/upload/presentation/view/upload_page.dart';
 import 'package:nigergram/features/video_feed/presentation/view/video_feed_view.dart';
 import 'package:go_router/go_router.dart';
 
-final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
-final GlobalKey<NavigatorState> _shellNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'shell');
+final GlobalKey<NavigatorState> _rootNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'root');
+final GlobalKey<NavigatorState> _shellNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'shell');
 
 class AppRouter {
   final router = GoRouter(
     navigatorKey: _rootNavigatorKey,
     initialLocation: '/login',
-    redirect: (context, state) {
-      final loggedIn = FirebaseAuth.instance.currentUser != null;
-      final onLogin = state.matchedLocation == '/login';
-      if (!loggedIn && !onLogin) return '/login';
-      if (loggedIn && onLogin) return RouterEnum.dashboardView.routeName;
-      return null;
-    },
     routes: [
       GoRoute(
         path: '/login',
         pageBuilder: (context, state) =>
             customPageBuilderWidget(context, state, const LoginPage()),
+      ),
+      GoRoute(
+        path: '/register',
+        pageBuilder: (context, state) =>
+            customPageBuilderWidget(context, state, const RegisterPage()),
       ),
       GoRoute(
         path: RouterEnum.uploadView.routeName,
