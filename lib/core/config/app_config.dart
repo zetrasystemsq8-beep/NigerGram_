3df856c1674faa0f9c99681e68b41c7cc59e46cf
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-06-23 10:39:18 +0100

    Update app_config.dart

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -4,13 +4,13 @@ class AppConfig {
   /// Monnify Sandbox API Key (Revealed from your dashboard)
   static const String monnifyApiKey = String.fromEnvironment(
     'MONNIFY_API_KEY',
-    defaultValue: 'YOUR_MONNIFY_API_KEY', // Swap this with your unhidden key for local device debugging
+    defaultValue: 'PTX5CMRZQA8JMAQWHYQPGMY3YGB5BWXN', // Swap this with your unhidden key for local device debugging
   );
 
   /// Monnify Sandbox Secret Key (Revealed from your dashboard)
   static const String monnifySecretKey = String.fromEnvironment(
     'MONNIFY_SECRET_KEY',
-    defaultValue: 'YOUR_MONNIFY_SECRET_KEY', // Swap this with your unhidden key for local device debugging
+    defaultValue: 'MK_TEST_BNZ3TVMA0H', // Swap this with your unhidden key for local device debugging
   );
 
   /// Monnify Sandbox Contract Code from Screenshot_20260623-072959.png
