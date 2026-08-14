ed9f00c0eb78376d5d3590507d852b4c2e53b2e8
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-08-14 10:12:25 +0100

    Enable core library desugaring in build.gradle.kts
    Enable core library desugaring for Java 11 compatibility and add the desugar library dependency.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -49,6 +50,10 @@ android {
             )
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
